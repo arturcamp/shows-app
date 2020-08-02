a6d3ceb4e8dc8a61cffcb9dcfc0cb8d36112d6fb
Artur Campos <arturc@ciandt.com>
2020-08-01 23:34:18 -0300

    Project creation

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack"
+import HomeScreen from "./src/screens/Home";
+
+function App(){
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Séries inc" component={HomeScreen}/>
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  )
+}
+
+export default App;
